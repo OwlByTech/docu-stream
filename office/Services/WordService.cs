da2d792ef586ee6ba7f64b2bb5a6c2a91223f790
camilo-camargo <camilocamargo49@gmail.com>
2024-08-03 02:02:09 -0500

    feat: add received and send docx by grpc

--- office/Services/WordService.cs
+++ office/Services/WordService.cs
@@ -1,8 +1,8 @@
-﻿using DocumentFormat.OpenXml.Drawing.Pictures;
-using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.Text.RegularExpressions;
 using Grpc.Core;
+using MimeDetective;
 
 namespace office.Services;
 
@@ -14,9 +14,8 @@ public class WordService : Word.WordBase
         _logger = logger;
     }
 
-    private void replaceStringValues(IEnumerable<Paragraph> elements, List<DocuStringValues> stringValues)
+    private void replaceStringValues(IEnumerable<Paragraph> elements, List<DocuValues> stringValues)
     {
-
         var textElements = elements
             .SelectMany(e => e.Elements<Run>())
             .SelectMany(r => r.Elements<Text>());
@@ -29,15 +28,63 @@ public class WordService : Word.WordBase
                 text.Text = regexPattern.Replace(text.Text, match => stringValue.Value);
             }
         }
-
     }
 
-    public override Task<WordApplyRes> Apply(WordApplyReq req, ServerCallContext context)
+    public override async Task Apply(IAsyncStreamReader<WordApplyReq> reqStream, IServerStreamWriter<WordApplyRes> resStream, ServerCallContext ctx)
     {
-        // TODO: Change test file for streaming by grpc
-        using (WordprocessingDocument wordDocument = WordprocessingDocument.Open("./test/Template.docx", true))
-        {
+        var bodyValues = new List<DocuValues>();
+        var headerValues = new List<DocuValues>();
+        MemoryStream chunks = new MemoryStream();
 
+        await foreach (var req in reqStream.ReadAllAsync())
+        {
+            switch (req.RequestCase)
+            {
+                case WordApplyReq.RequestOneofCase.Word:
+                    bodyValues.AddRange(req.Word.Body);
+                    headerValues.AddRange(req.Word.Header);
+                    break;
+
+                case WordApplyReq.RequestOneofCase.Docu:
+                    var chunk = req.Docu.Chunk;
+                    if (chunk.Length <= 0)
+                    {
+                        continue;
+                    }
+
+                    await chunks.WriteAsync(chunk.ToByteArray(), 0, chunk.Length);
+                    break;
+
+                default:
+                    throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request type"));
+            }
+        }
+
+        long unixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        var Inspector = new ContentInspectorBuilder()
+        {
+            Definitions = MimeDetective.Definitions.Default.All()
+        }.Build();
+
+        var Results = Inspector.Inspect(chunks.ToArray());
+        var extension = Results.ByFileExtension().First().Extension;
+
+        if (extension != "docx")
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, $"Invalid file type: {extension}. Only '.docx' files are allowed."));
+        }
+
+        string docuPath = $"/tmp/{unixTime}.{extension}";
+        using (var fileStream = new FileStream(docuPath, FileMode.Create, FileAccess.Write))
+        {
+            chunks.WriteTo(fileStream);
+        }
+        chunks.SetLength(0);
+        chunks.Position = 0;
+
+        using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(docuPath, true))
+        {
             MainDocumentPart? mainDocumentPart = wordDocument.MainDocumentPart;
             if (mainDocumentPart == null)
             {
@@ -53,12 +100,8 @@ public class WordService : Word.WordBase
             foreach (var headerPart in mainDocumentPart.HeaderParts)
             {
                 var header = headerPart.Header;
-                if (header == null)
-                {
-                    continue;
-                }
-
-                replaceStringValues(header.Elements<Paragraph>(), req.Header.ToList());
+                if (header == null) { continue; }
+                replaceStringValues(header.Elements<Paragraph>(), bodyValues);
             }
 
             Body? body = document.Body;
@@ -67,11 +110,30 @@ public class WordService : Word.WordBase
                 throw new RpcException(new Status(StatusCode.NotFound, "body required."));
             }
 
-            replaceStringValues(body.Elements<Paragraph>(), req.Body.ToList());
+            replaceStringValues(body.Elements<Paragraph>(), headerValues);
 
             document.Save();
         }
-        return Task.FromResult(new WordApplyRes { });
+
+        using (var stream = new FileStream(docuPath, FileMode.Open, FileAccess.Read))
+        {
+            stream.CopyTo(chunks);
+        }
+
+        if (!File.Exists(docuPath))
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "File was not created successfully."));
+        }
+
+        File.Delete(docuPath);
+
+        await resStream.WriteAsync(new WordApplyRes
+        {
+            Docu = new DocuChunk
+            {
+                Chunk = Google.Protobuf.ByteString.CopyFrom(chunks.ToArray())
+            }
+        });
     }
 }
 
