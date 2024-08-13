ad950a296fb824679ece51438045ed16ea13d56c
camilo-camargo <camilocamargo49@gmail.com>
2024-08-13 17:14:58 -0500

    feat: replace header and body images

--- office/Services/WordService.cs
+++ office/Services/WordService.cs
@@ -1,8 +1,11 @@
 ﻿using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Drawing.Pictures;
 using System.Text.RegularExpressions;
 using Grpc.Core;
 using MimeDetective;
+using Google.Protobuf.Collections;
+using Google.Protobuf;
 
 namespace office.Services;
 
@@ -14,7 +17,7 @@ public class WordService : Word.WordBase
         _logger = logger;
     }
 
-    private void replaceStringValues(IEnumerable<Paragraph> elements, List<DocuValues> stringValues)
+    private void replaceTextValues(IEnumerable<Paragraph> elements, IEnumerable<DocuValue> textValues)
     {
         var textElements = elements
             .SelectMany(e => e.Elements<Run>())
@@ -22,19 +25,58 @@ public class WordService : Word.WordBase
 
         foreach (var text in textElements)
         {
-            foreach (var stringValue in stringValues)
-            { 
+            foreach (var stringValue in textValues)
+            {
                 var regexPattern = new Regex(@"\{\{\s*\b" + Regex.Escape(stringValue.Key) + @"\b\s*\}\}", RegexOptions.Compiled);
                 text.Text = regexPattern.Replace(text.Text, match => stringValue.Value);
             }
         }
     }
 
+    private void replaceImageValues(IEnumerable<Drawing> drawings, OpenXmlPart part, IEnumerable<DocuValue> imageValues, List<MemoryStream> attachFiles)
+    {
+        foreach (var drawing in drawings)
+        {
+            var drawParts = drawing.Descendants<NonVisualDrawingProperties>().FirstOrDefault();
+            if (drawParts == null)
+            {
+                continue;
+            }
+
+            var imageDescription = drawParts?.Description?.Value;
+            if (imageDescription == null)
+            {
+                continue;
+            }
+
+            var imageFound = imageValues.Where(h => "{{" + h.Key + "}}" == imageDescription).ToList();
+            if (imageFound.Count() == 0)
+            {
+                continue;
+            }
+
+            var blipFill = drawing.Descendants<BlipFill>().FirstOrDefault();
+            var blipEmbedValue = blipFill?.Blip?.Embed?.Value;
+            if (blipEmbedValue == null) { continue; }
+
+            var imagePart = part.GetPartById(blipEmbedValue) as ImagePart;
+            if (imagePart == null) { continue; }
+
+            var imageIndex = int.Parse(imageFound[0].Value);
+            using (var image = attachFiles[imageIndex])
+            {
+                image.Position = 0;
+                imagePart.FeedData(image);
+            }
+        }
+
+    }
+
     public override async Task Apply(IAsyncStreamReader<WordApplyReq> reqStream, IServerStreamWriter<WordApplyRes> resStream, ServerCallContext ctx)
     {
-        var bodyValues = new List<DocuValues>();
-        var headerValues = new List<DocuValues>();
-        MemoryStream chunks = new MemoryStream();
+        var bodyValues = new List<DocuValue>();
+        var headerValues = new List<DocuValue>();
+        List<MemoryStream> attachFiles = new List<MemoryStream>();
 
         await foreach (var req in reqStream.ReadAllAsync())
         {
@@ -46,13 +88,24 @@ public class WordService : Word.WordBase
                     break;
 
                 case WordApplyReq.RequestOneofCase.Docu:
-                    var chunk = req.Docu.Chunk;
-                    if (chunk.Length <= 0)
+                    var chunks = req.Docu.Chunks;
+                    if (attachFiles.Count != chunks.Count)
                     {
-                        continue;
+                        for (int i = 0; i < chunks.Count; i++)
+                        {
+                            attachFiles.Add(new MemoryStream());
+                        }
                     }
 
-                    await chunks.WriteAsync(chunk.ToByteArray(), 0, chunk.Length);
+                    for (int i = 0; i < chunks.Count; i++)
+                    {
+                        if (chunks[i].Length <= 0)
+                        {
+                            continue;
+                        }
+
+                        await attachFiles[i].WriteAsync(chunks[i].ToByteArray(), 0, chunks[i].Length);
+                    }
                     break;
 
                 default:
@@ -67,7 +120,7 @@ public class WordService : Word.WordBase
             Definitions = MimeDetective.Definitions.Default.All()
         }.Build();
 
-        var Results = Inspector.Inspect(chunks.ToArray());
+        var Results = Inspector.Inspect(attachFiles[0].ToArray());
         var extension = Results.ByFileExtension().First().Extension;
 
         if (extension != "docx")
@@ -78,10 +131,10 @@ public class WordService : Word.WordBase
         string docuPath = $"/tmp/{unixTime}.{extension}";
         using (var fileStream = new FileStream(docuPath, FileMode.Create, FileAccess.Write))
         {
-            chunks.WriteTo(fileStream);
+            attachFiles[0].WriteTo(fileStream);
         }
-        chunks.SetLength(0);
-        chunks.Position = 0;
+        attachFiles[0].SetLength(0);
+        attachFiles[0].Position = 0;
 
         using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(docuPath, true))
         {
@@ -91,17 +144,24 @@ public class WordService : Word.WordBase
                 throw new RpcException(new Status(StatusCode.NotFound, "main document part required."));
             }
 
-            Document document = mainDocumentPart.Document;
-            if (document == null)
-            {
-                throw new RpcException(new Status(StatusCode.NotFound, "document required."));
-            }
+
+
+            var headerImages = headerValues.Where(h => h.Type == DocuValueType.Image);
+            var headerTexts = headerValues.Where(h => h.Type == DocuValueType.Text);
 
             foreach (var headerPart in mainDocumentPart.HeaderParts)
             {
                 var header = headerPart.Header;
                 if (header == null) { continue; }
-                replaceStringValues(header.Elements<Paragraph>(), headerValues);
+
+                replaceTextValues(header.Elements<Paragraph>(), headerTexts);
+                replaceImageValues(headerPart.Header.Descendants<Drawing>(), headerPart, headerImages, attachFiles);
+            }
+
+            Document document = mainDocumentPart.Document;
+            if (document == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "document required."));
             }
 
             Body? body = document.Body;
@@ -110,14 +170,17 @@ public class WordService : Word.WordBase
                 throw new RpcException(new Status(StatusCode.NotFound, "body required."));
             }
 
-            replaceStringValues(body.Elements<Paragraph>(), bodyValues);
+            var bodyTexts = bodyValues.Where(b => b.Type == DocuValueType.Text);
+            var bodyImages = bodyValues.Where(b => b.Type == DocuValueType.Image);
+            replaceTextValues(body.Elements<Paragraph>(), bodyTexts);
+            replaceImageValues(body.Descendants<Drawing>(), mainDocumentPart, bodyImages, attachFiles);
 
             document.Save();
         }
 
         using (var stream = new FileStream(docuPath, FileMode.Open, FileAccess.Read))
         {
-            stream.CopyTo(chunks);
+            stream.CopyTo(attachFiles[0]);
         }
 
         if (!File.Exists(docuPath))
@@ -127,12 +190,13 @@ public class WordService : Word.WordBase
 
         File.Delete(docuPath);
 
+        RepeatedField<ByteString> chunksRes = new RepeatedField<ByteString>();
+        var docu = new DocuChunk();
+        docu.Chunks.Add(ByteString.CopyFrom(attachFiles[0].ToArray()));
+
         await resStream.WriteAsync(new WordApplyRes
         {
-            Docu = new DocuChunk
-            {
-                Chunk = Google.Protobuf.ByteString.CopyFrom(chunks.ToArray())
-            }
+            Docu = docu
         });
     }
 }
